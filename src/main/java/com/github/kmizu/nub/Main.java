18fbd845409d65b9b3c0b2b0fcda80d7542d6546
Kota Mizushima <mizukota@gmail.com>
2016-12-13 04:54:45 +0900

    Extract method

--- src/main/java/com/github/kmizu/nub/Main.java
+++ src/main/java/com/github/kmizu/nub/Main.java
@@ -8,18 +8,25 @@ import java.io.IOException;
 import java.io.StringReader;
 
 public class Main {
+    private static CommonTokenStream streamFrom(String input) throws IOException {
+        ANTLRInputStream antlrStream = new ANTLRInputStream(new StringReader(input));
+        NubLexer lexer = new NubLexer(antlrStream);
+        return new CommonTokenStream(lexer);
+    }
+    private static CommonTokenStream streamFrom(File file) throws IOException {
+        ANTLRInputStream antlrStream = new ANTLRInputStream(new FileInputStream(file));
+        NubLexer lexer = new NubLexer(antlrStream);
+        return new CommonTokenStream(lexer);
+    }
     public static void main(String[] args) throws IOException{
         if(args.length == 0) {
-            ANTLRInputStream input = new ANTLRInputStream(new StringReader("def printRange(from, to) { let i = from; while(i < to) { println(i); i = i + 1; } } printRange(1, 10); // Loop"));
-            NubLexer lexer = new NubLexer(input);
-            CommonTokenStream stream = new CommonTokenStream(lexer);
+            CommonTokenStream stream = streamFrom("def printRange(from, to) { let i = from; while(i < to) { println(i); i = i + 1; } } printRange(1, 10); // Loop");
             AstNode.ExpressionList program = new NubParser(stream).program().e;
             Evaluator evaluator = new Evaluator();
             evaluator.evaluate(program);
         } else {
             String fileName = args[0];
-            ANTLRInputStream antlrInput = new ANTLRInputStream(new FileInputStream(new File(fileName)));
-            CommonTokenStream stream = new CommonTokenStream(new NubLexer(antlrInput));
+            CommonTokenStream stream = streamFrom(new File(fileName));
             NubParser parser = new NubParser(stream);
             AstNode.ExpressionList program = parser.program().e;
             Evaluator evaluator = new Evaluator();
